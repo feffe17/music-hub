e2233f7698b9adb8c763d8ae3657f69c1debba47
feffe1702 <fpm.asd@gmail.com>
2025-03-30 18:58:09 +0200

    Add API routes for Albums, Songs, and Genres with CRUD operations and public access

--- BackOffice/routes/api.php
+++ BackOffice/routes/api.php
@@ -1,8 +1,16 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\AlbumController;
+use App\Http\Controllers\Api\SongController;
+use App\Http\Controllers\Api\GenreController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Rotte pubbliche accessibili dal frontend React (solo GET)
+Route::get('albums', [AlbumController::class, 'index']);
+Route::get('albums/{id}', [AlbumController::class, 'show']);
+
+Route::get('songs', [SongController::class, 'index']);
+Route::get('songs/{id}', [SongController::class, 'show']);
+
+Route::get('genres', [GenreController::class, 'index']);
+Route::get('genres/{id}', [GenreController::class, 'show']);
--- BackOffice/routes/web.php
+++ BackOffice/routes/web.php
@@ -1,10 +1,28 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Api\AlbumController;
+use App\Http\Controllers\Api\SongController;
+use App\Http\Controllers\Api\GenreController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+// Rotte protette con autenticazione
+Route::middleware('auth:sanctum')->group(function () {
+
+    // Gestione degli album
+    Route::post('albums', [AlbumController::class, 'store']);
+    Route::put('albums/{id}', [AlbumController::class, 'update']);
+    Route::delete('albums/{id}', [AlbumController::class, 'destroy']);
+
+    // Gestione delle canzoni
+    Route::post('songs', [SongController::class, 'store']);
+    Route::put('songs/{id}', [SongController::class, 'update']);
+    Route::delete('songs/{id}', [SongController::class, 'destroy']);
+
+    // Gestione dei generi
+    Route::post('genres', [GenreController::class, 'store']);
+    Route::put('genres/{id}', [GenreController::class, 'update']);
+    Route::delete('genres/{id}', [GenreController::class, 'destroy']);
 });
 
 Route::get('/dashboard', function () {
@@ -17,4 +35,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
