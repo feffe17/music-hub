74a92c803e53dcfaae6cd12ac85d3b2d297904c5
feffe1702 <fpm.asd@gmail.com>
2025-03-30 18:20:11 +0200

    Add Album, Genre, and Song models with relationships and fillable attributes

---
+++ BackOffice/app/Models/Song.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Song extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['title', 'artist', 'album_id', 'genre_id', 'release_date', 'youtube_link', 'spotify_link'];
+
+    public function album()
+    {
+        return $this->belongsTo(Album::class);
+    }
+
+    public function genre()
+    {
+        return $this->belongsTo(Genre::class);
+    }
+}
