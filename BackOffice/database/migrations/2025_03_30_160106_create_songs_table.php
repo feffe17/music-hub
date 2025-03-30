02dd27aaf621a38cced1915f0cb5664297c75920
feffe1702 <fpm.asd@gmail.com>
2025-03-30 18:05:06 +0200

    Add migrations for genres, albums, and songs tables

---
+++ BackOffice/database/migrations/2025_03_30_160106_create_songs_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('songs', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('artist');
+            $table->foreignId('album_id')->nullable()->constrained()->onDelete('cascade');
+            $table->foreignId('genre_id')->constrained()->onDelete('cascade');
+            $table->date('release_date')->nullable();
+            $table->string('youtube_link')->nullable();
+            $table->string('spotify_link')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('songs');
+    }
+};
