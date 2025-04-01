bbf2b8e1e1cab3cd007bd7e0a8c4862e095d62eb
feffe1702 <fpm.asd@gmail.com>
2025-04-01 15:06:57 +0200

    Add UserSeeder to populate admin user in the database

--- BackOffice/database/seeders/DatabaseSeeder.php
+++ BackOffice/database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         $this->call([
+            UserSeeder::class,
             GenreSeeder::class,
             AlbumSeeder::class,
             SongSeeder::class,
