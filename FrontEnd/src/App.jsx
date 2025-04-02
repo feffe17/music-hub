5f051b34b7fb122053fd58ddf0a4ae17c2e8f874
feffe1702 <fpm.asd@gmail.com>
2025-04-02 11:03:55 +0200

    Implement routing and layout structure with GlobalProvider; add Home, AlbumDetails, and SongDetails pages

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,12 +1,28 @@
 import { BrowserRouter as Router, Route, Routes, BrowserRouter } from 'react-router-dom'
 import './App.css'
+import { GlobalProvider } from './context/GlobalContext'
+import DefaultLayout from './components/layouts/DefaultLayout'
+import Home from './pages/Home'
+import Albums from "./Pages/AlbumDetails"
+import Songs from "./Pages/SongDetails"
 
 function App() {
 
 
   return (
     <>
-
+      <GlobalProvider>
+        <BrowserRouter >
+          <Routes>
+            <Route element={<DefaultLayout />} >
+              <Route path="/" element={<Home />} />
+              <Route path="/album/:albumName" element={<Albums />} />
+              <Route path="/song/:songName" element={<Songs />} />
+              {/* <Route path="/genres" element={<Genres />} /> */}
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </GlobalProvider>
     </>
   )
 }
