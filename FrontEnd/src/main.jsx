6769b1b09b8bd010a33a66817e918a67d56be766
feffe1702 <fpm.asd@gmail.com>
2025-04-02 10:21:24 +0200

    Update index.html and App components for Music Hub branding; add favicon and Bootstrap integration

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import 'bootstrap/dist/js/bootstrap.min.js'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap-icons/font/bootstrap-icons.min.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
